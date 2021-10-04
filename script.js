bf738345bffa023e1c2e2d419a08a7f17d8df408
adamteddychang <adamchang0725@gmail.com>
2021-10-04 17:27:08 +0200

    Fixed hamburger menu button

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function toggleMenu() {
   if (navMenu.classList.contains('show')) {
     navMenu.classList.remove('show');
     closeIcon.style.display = 'none';
-    menuIcon.style.display = 'flex';
+    menuIcon.style.display = 'inline';
   } else {
     navMenu.classList.add('show');
-    closeIcon.style.display = 'flex';
+    closeIcon.style.display = 'inline';
     menuIcon.style.display = 'none';
   }
 }
