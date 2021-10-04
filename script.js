16f75da58972bfa7ed0474a1f74b9df7392913c5
adamteddychang <adamchang0725@gmail.com>
2021-10-04 16:06:14 +0200

    Styled menu button

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const listItems = document.querySelectorAll('.nav-list-items')
 const hamburger = document.querySelector('#menu-btn');
 const nav_menu = document.querySelector('.nav-list')
-const nav_list_items = document.querySelectorAll('.nav-list-items')
+const nav_list_items = document.querySelectorAll('a .nav-list-items')
 const menuIcon = document.querySelector('.menu-icon')
 const closeIcon = document.querySelector('.close-icon')
 
-nav_menu.innerHTML = `
-    <li class="nav-list-items"><a href="">Portfolio</a></li>
-    <li class="nav-list-items"><a href="">About</a></li>
-    <li class="nav-list-items"><a href="">Contact</a></li>
-    <li class="nav-list-items"><a href=""><img src="Images/mail.svg" alt=""></a></li>       
-`
+
 
 function toggleMenu() {
     if (nav_menu.classList.contains("show")) {
@@ -19,10 +14,11 @@ function toggleMenu() {
         menuIcon.style.display = "block";
     } else {
         nav_menu.classList.add("show");
-        closeIcon.style.display = "block";
+        closeIcon.style.display = "flex";
         menuIcon.style.display = "none";
     }
 }
+
 
 hamburger.addEventListener("click", toggleMenu);
 
