199e02faefc116dd4fcdc2c1884fff1891628a83
adamteddychang <adamchang0725@gmail.com>
2021-10-04 16:39:19 +0200

    Fixed button styling, fixed toggle

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const listItems = document.querySelectorAll('.nav-list-items')
 const hamburger = document.querySelector('#menu-btn');
-const nav_menu = document.querySelector('.nav-list')
-const nav_list_items = document.querySelectorAll('a .nav-list-items')
-const menuIcon = document.querySelector('.menu-icon')
-const closeIcon = document.querySelector('.close-icon')
+const navMenu = document.querySelector('.nav-list');
 
-
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const navA = document.querySelectorAll('.nav_a');
 
 function toggleMenu() {
-    if (nav_menu.classList.contains("show")) {
-        nav_menu.classList.remove("show");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        nav_menu.classList.add("show");
-        closeIcon.style.display = "flex";
-        menuIcon.style.display = "none";
-    }
+  if (navMenu.classList.contains('show')) {
+    navMenu.classList.remove('show');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'flex';
+  } else {
+    navMenu.classList.add('show');
+    closeIcon.style.display = 'flex';
+    menuIcon.style.display = 'none';
+  }
 }
 
+hamburger.addEventListener('click', toggleMenu);
 
-hamburger.addEventListener("click", toggleMenu);
-
-
-
-   
+navA.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
